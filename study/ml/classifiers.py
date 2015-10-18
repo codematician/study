f9de65104f03ede429e867fc879dcd7e841695e3
Andy R. Terrel <andy.terrel@gmail.com>
2015-10-18 07:34:16 -0500

    Adding a lookup classifier, minor format fixes
    The lookup classifier is another dummy classifier proposed in Russell and
    Norvig's AIMA book [Russel Norvig 1995]. It is another way to build a
    simple classifier to measure more sophisticated classifiers against.
    
    This pull request also includes some minor formatting fixes to adhere
    to the PEP 8 style guide and DRY principle.

--- study/ml/classifiers.py
+++ study/ml/classifiers.py
@@ -1,8 +1,18 @@
 from abc import ABCMeta, abstractmethod
+import operator
+from functools import reduce
 
 
 class Classifier(metaclass=ABCMeta):
+    """A abstrace base class for classifiers.
 
+    Classifiers consist of two functions, fit and predict.
+
+    - fit: The fit method is called with a Pandas Dataframe of labeled training
+           data. The prediction field indicates the label that is to be predicted.
+
+    - predict: Takes single row of data and produces the field trained to fit.
+    """
     @abstractmethod
     def fit(self, df, predict_field="class"):
         pass
@@ -12,10 +22,45 @@ class Classifier(metaclass=ABCMeta):
         pass
 
 
-class MajorityClassifier(Classifier):
-    """A classifer that always give back the majority answer of the training set.
+class LookUpClassifier(Classifier):
+    """A classifier that looks up an answer in a training set.
 
-     This classifer is useful for comparing the performance of a bad model.
+    Notes
+    -----
+
+     This classifier is only useful for comparing the performance of a bad model.
+
+    References
+    ----------
+    - Russell, Stuart, and Peter Norvig. "Artificial intelligence: a modern approach." (1995). APA
+
+    """
+    def __init__(self):
+        self._lookup_df = None
+        self._predict_field = None
+        self._predict_default = None
+
+    def fit(self, df, predict_field="class"):
+        self._predict_field = predict_field
+        self._predict_default = df[predict_field].mode()[0]
+        self._lookup_df = df.drop_duplicates()
+        return True
+
+    def predict(self, x):
+        criterion = reduce(operator.and_, map(lambda idx: self._lookup_df[idx] == x[idx], x.index))
+        lookup = self._lookup_df[criterion]
+        predict_val = self._predict_default if len(lookup) == 0 else lookup[self._predict_field].iloc[0]
+        return predict_val
+
+
+class MajorityClassifier(Classifier):
+    """A classifier that always give back the majority answer of the training set.
+
+    Notes
+    -----
+
+     This classifier is only useful for comparing the performance of a bad model.
+
     """
     def __init__(self):
         self._prediction_value = None
--- study/ml/tests/test_classifiers.py
+++ study/ml/tests/test_classifiers.py
@@ -2,14 +2,49 @@ import unittest
 
 import pandas as pd
 
-from study.ml.classifiers import MajorityClassifier
+from study.ml.classifiers import LookUpClassifier, MajorityClassifier
 
 
-class TestMajorityClassifier(unittest.TestCase):
+class ClassifierBaseTest(unittest.TestCase):
+
+    data1_df = pd.DataFrame({'one': [1., 2., 3., 4.],
+                             'two': [1., 3., 2., 1.]})
+
+
+class TestLookupClassifier(ClassifierBaseTest):
+    """Test the LookupClassifier"""
+
+    def test_creation(self):
+        """Test creation of lookup classifier"""
+        classifier = LookUpClassifier()
+        self.assertTrue(classifier)
+
+    def test_fit(self):
+        """Test fit method of lookup classifier"""
+        fit_res = LookUpClassifier().fit(self.data1_df, "two")
+        self.assertTrue(fit_res)
+
+    def test_prediction_lookup(self):
+        """Test predict method of lookup classifier"""
+        classifier = LookUpClassifier()
+        predict_field = "two"
+        classifier.fit(self.data1_df, predict_field)
+        predict_input = pd.Series(data=[2., ], index=["one", ])
+        prediction = classifier.predict(predict_input)
+        self.assertEqual(prediction, self.data1_df[self.data1_df["one"] == 2.][predict_field].iloc[0])
+
+    def test_prediction_default(self):
+        """Test predict method of lookup classifier"""
+        classifier = LookUpClassifier()
+        predict_field = "two"
+        classifier.fit(self.data1_df, predict_field)
+        predict_input = pd.Series(data=[5., ], index=["one"])
+        prediction = classifier.predict(predict_input)
+        self.assertEqual(prediction, self.data1_df[predict_field].mode()[0])
+
+
+class TestMajorityClassifier(ClassifierBaseTest):
     """Test the MajoricyClassifier"""
-
-    data_df = pd.DataFrame({'one': [1., 2., 3., 4.],
-                            'two': [1., 3., 2., 1.]})
 
     def test_creation(self):
         """Test creation of majority classifier"""
@@ -17,15 +52,18 @@ class TestMajorityClassifier(unittest.TestCase):
         self.assertTrue(classifier)
 
     def test_fit(self):
-        """Test fit method of majoricy classifier"""
-        fit_res = MajorityClassifier().fit(self.data_df, "two")
+        """Test fit method of majority classifier"""
+        fit_res = MajorityClassifier().fit(self.data1_df, "two")
         self.assertTrue(fit_res)
 
     def test_prediction(self):
+        """Test predict method of majority classifier"""
         classifier = MajorityClassifier()
-        classifier.fit(self.data_df, "two")
-        prediction = classifier.predict([1., ])
-        self.assertEqual(prediction, self.data_df['two'].mode()[0])
+        predict_field = "two"
+        classifier.fit(self.data1_df, predict_field)
+        predict_input = pd.Series([1., ], index=["one", ])
+        prediction = classifier.predict(predict_input)
+        self.assertEqual(prediction, self.data1_df[predict_field].mode()[0])
 
 
 if __name__ == '__main__':

793ac861d1b187c7b6ef2998ddafec8e383d3a53
Andy R. Terrel <andy.terrel@gmail.com>
2015-10-30 10:04:03 -0500

    Adding Adult dataset from UCI to test classification with numerical attributes

--- study/datasets/__init__.py
+++ study/datasets/__init__.py
@@ -2,8 +2,13 @@ import os
 
 import pandas as pd
 
-print(os.path.dirname(__file__))
 
-cars_df = pd.read_csv(os.path.dirname(__file__)+"/car/car.data",
+cars_df = pd.read_csv(os.path.join(os.path.dirname(__file__), "car", "car.data"),
                       names=["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"],
                       index_col=False)
+
+adult_df = pd.read_csv(os.path.join(os.path.dirname(__file__), "adult", "adult.data"),
+                       names=["age", "workclass","fnlwgt", "education", "education-num", "marital-status"," occupation",
+                              "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week",
+                              "native-country", "class"],
+                       index_col=False)
